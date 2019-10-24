37b06c28acc6ea3848b58a489b029c5d60177ed4
matanceros <56916698+matanceros@users.noreply.github.com>
2019-10-24 19:15:48 +0800

    CN support for pdf output

--- conf.py
+++ conf.py
@@ -55,3 +55,12 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for PDF output -------------------------------------------------
+
+# use xelatex to override original latex engine for CN support
+latex_engine = 'xelatex'
+latex_use_xindy = False
+latex_elements = {
+    'preamble': '\\usepackage[UTF8]{ctex}\n',
+}
